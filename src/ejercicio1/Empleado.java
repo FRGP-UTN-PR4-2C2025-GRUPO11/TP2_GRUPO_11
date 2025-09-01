57a669ab2cbb95c01ca53268ca8291f810f27b6c
martinrodri1 <martorodri14@gmail.com>
2025-09-01 12:12:26 -0300

    Agregue el hashCode y equals

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -119,5 +119,24 @@ public class Empleado extends Persona implements Comparable<Empleado> {
 			return 0;
 		}
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(legajo, puesto);
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		return legajo == other.legajo && Objects.equals(puesto, other.puesto);
+	}
+	
 }
