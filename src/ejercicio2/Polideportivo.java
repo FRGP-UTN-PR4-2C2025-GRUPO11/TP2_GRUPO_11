fdff866c86aa67ec7e08b526e72d0079ea220d5a
GabrielErizaga <gabriel.erizaga@alumnos.frgp.utn.edu.ar>
2025-09-03 11:55:56 -0300

    IEdificio y Polideportivo

---
+++ src/ejercicio2/Polideportivo.java
@@ -0,0 +1,12 @@
+package ejercicio2;
+
+public class Polideportivo implements IEdificio{
+
+	private double superficie;
+
+	@Override
+	public double getSuperficieEdificio() {
+		return superficie;
+	}
+	
+}
