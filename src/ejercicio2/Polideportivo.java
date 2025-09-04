3fdf069ced322e5ff7b0d3d35a9cd970e7422a45
Mariano Belgrano <mbelgrano@gmail.com>
2025-09-04 12:27:19 -0300

    Aporte 1 Mariano: Agregué String nombre a la clase Polideportivo, y separe superficie en superficieEdificio y superficie del polideportivo. Tambien agregue todos los getters y setters.

--- src/ejercicio2/Polideportivo.java
+++ src/ejercicio2/Polideportivo.java
@@ -3,10 +3,38 @@ package ejercicio2;
 public class Polideportivo implements IEdificio{
 
 	private double superficie;
+	private double superficieEdificio;
+	String nombre;
+
+	
+	public double getSuperficie() {
+		return superficie;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+
+
+	public void setSuperficieEdificio(double superficieEdificio) {
+		this.superficieEdificio = superficieEdificio;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 
 	@Override
 	public double getSuperficieEdificio() {
-		return superficie;
+		return superficieEdificio;
 	}
 	
 }
