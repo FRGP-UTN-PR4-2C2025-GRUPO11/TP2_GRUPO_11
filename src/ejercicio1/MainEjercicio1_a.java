1d8bceb8e9c7f1dc0481cae2c18816d20598396a
GabrielErizaga <gabriel.erizaga@alumnos.frgp.utn.edu.ar>
2025-09-04 15:11:41 -0300

    Ejercicio D try catch

--- src/ejercicio1/MainEjercicio1_a.java
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -6,12 +6,19 @@ public class MainEjercicio1_a {
 
 	public static void main(String[] args) {
 		Persona obj = new Persona();
+		Persona obj2 = new Persona();
 		try{
 			obj.setDni("AA202020");
+			obj2.setDni("20202020");
 			Persona.verificarDNI(obj.getDni());
-			System.out.println("Funciona bien");
+			Persona.verificarDNI(obj2.getDni());
+			System.out.println("Persona agregada correctamente");
 			}
-		finally {}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Persona no agregada por no verificar el DNI");
+		}
+		
 		
 	}
 
